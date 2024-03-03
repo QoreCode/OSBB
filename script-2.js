d1b3dcf2ae36c0f1847626109f69d587aa59001b
Dmitrij <d.develop@protonmail.com>
2024-03-03 14:58:23 +0200

    Result

--- script-2.js
+++ script-2.js
@@ -9,19 +9,22 @@ function register(id) {
         modal.style.display = "flex";
         setTimeout(() => {
             modal.style.opacity = "1";
-            
+
             modalImg.src = this.src;
             modalImg.alt = this.alt;
             zoomEl.style.backgroundImage = `url('${this.src}')`;
+
+            if (img.offsetWidth > img.offsetHeight) {
+                zoomEl.classList.add("zoom-width");
+            }
         })
-        
+
         document.body.style.overflowY = "hidden";
         document.body.style.paddingRight = "15px";
     }
 }
 
 function zoom(e) {
-    console.log('lol3');
     var zoomer = e.currentTarget;
     e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
     e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
@@ -34,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
     modal = document.getElementById('myModal');
     modalImg = document.getElementById("img01");
     zoomEl = document.getElementById("zoom");
-    
+
     zoomEl.onmousemove = zoom;
 
     // When the user clicks on <span> (x), close the modal
@@ -43,6 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(function () {
             modal.style.display = "none";
             img01.className = "modal-content";
+            zoomEl.classList.remove("zoom-width");
         }, 400);
 
         modal.style.opacity = "0";
@@ -50,3 +54,19 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.style.paddingRight = "0";
     }
 })
+
+
+function copyToClipboard() {
+    var text = "5167 8032 7096 3486";
+    navigator.clipboard.writeText(text).then(function () {
+        Toastify({
+            text: "Скопійовано",
+            className: "info",
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            }
+        }).showToast();
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
